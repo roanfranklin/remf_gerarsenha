5981ce932f59a9d80e9a2de29c62ed015b55f1bc
Roan Franklin <roanfranklin@gmail.com>
2020-08-08 10:47:47 -0300

    commit inicial.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# remf_gerarsenha
+## REMF_GerarSenha :
+[![Github All Releases](https://img.shields.io/badge/REMF_GerarSenha-versão%200.0.1-red)]()
+[![Github All Releases](https://img.shields.io/badge/suporte-python%203.7%2F3.8%20%2B-brightgreen)]()
+[![Github All Releases](https://img.shields.io/badge/platforma-windows%20%7C%20linux-lightgrey)]()
+
 Gerar senha randomica informando os tipos de caracteres e qual o tamanho.
+
+## Instalação :
+Use o gerenciado de pacotes [pip](https://pip.pypa.io/en/stable/) para instalar requisitos
+```bash
+cd remf_gerarsenha
+python3 -m pip install -r requirements.txt
+python3 remf_gerarsenha.py 
+```
+
+## Ajuda :
+```bash
+./remf_gerarsenha.py 1-6 [ TAMANHO ]
+./remf_gerarsenha.py 0 [ TAMANHO ] "CARACTERES"
+```
+os tipos:
+0 = PRECISA SETAR OS CARACTERES NO 3º CAMPO.
+1 = ALFABETO + NUMEROS + CARACTERES ESPECIAIS
+2 = ALFABETO
+3 = ALFABETO + NUMEROS
+4 = ALFABETO + CARACTERES ESPECIAIS
+5 = NUMEROS + CARACTERES ESPECIAIS
+6 = CARACTERES ESPECIAIS
+* = NUMEROS
+
+<small>* qualquer outro numero.</small> 
+
+## Roan Franklin :
+http://www.remf.com.br/
---
+++ remf_gerarsenha.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+# pip3 install pyfiglet
+# pip3 install colorama
+
+import os, sys, string
+import pyfiglet
+import colorama
+from colorama import Fore, Style
+from random import random, choice
+
+def enumero(valor):
+    try:
+         float(valor)
+    except ValueError:
+         return False
+    return True
+
+def ajuda():
+    print(' '+Fore.CYAN)
+    print(' Use:', sys.argv[0], '1-6 [ TAMANHO ]')
+    print(' Use:', sys.argv[0], '0 [ TAMANHO ] "CARACTERES"')
+    print('\n')
+    print('       1 = ALFABETO + NUMEROS + CARACTERES ESPECIAIS')
+    print('       2 = ALFABETO')
+    print('       3 = ALFABETO + NUMEROS')
+    print('       4 = ALFABETO + CARACTERES ESPECIAIS')
+    print('       5 = NUMEROS + CARACTERES ESPECIAIS')
+    print('       6 = CARACTERES ESPECIAIS')
+    print('         = NUMEROS')
+    print(Style.RESET_ALL+' ')
+
+def checar_char(tipo):
+    if tipo == 1:
+        valores = string.ascii_letters
+        valores += string.digits
+        valores += string.punctuation
+    elif tipo == 2:
+        valores = string.ascii_letters
+    elif tipo == 3:
+        valores = string.ascii_letters
+        valores += string.digits
+    elif tipo == 4:
+        valores = string.ascii_letters
+        valores += string.punctuation
+    elif tipo == 5:
+        valores = string.digits
+        valores += string.punctuation
+    elif tipo == 6:
+        valores = string.punctuation
+    else:
+        valores = string.digits
+
+    return valores
+
+def gerar_senha(t, tamanho, caracteres):
+    if not enumero(t) or not enumero(tamanho):
+        ajuda()
+        quit()
+
+    if int(t) == 0:
+        if len(caracteres) > 0:
+            valores = caracteres
+        else:
+            ajuda()
+            quit()
+    else:
+        valores = checar_char(int(t))
+        
+    senha = ''
+
+    for i in range(int(tamanho)):
+        senha += choice(valores)
+
+    return senha
+    
+
+def main():
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 4:
+            print(gerar_senha(sys.argv[1], sys.argv[2], sys.argv[3]))
+        elif len(sys.argv) == 3:
+            print(gerar_senha(sys.argv[1], sys.argv[2], ''))
+        else:
+            ajuda()
+    else:
+        ajuda()
+
+if __name__ == "__main__":
+    main()
